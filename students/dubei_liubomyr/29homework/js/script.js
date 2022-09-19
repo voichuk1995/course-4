937a5628f75edc13b6d15fd91ca22b9751009290
Liubomyr Dubei <Liubomydubei@gmail.com>
2022-09-19 22:24:25 +0300

    new console log

--- students/dubei_liubomyr/29homework/js/script.js
+++ students/dubei_liubomyr/29homework/js/script.js
@@ -9,7 +9,8 @@ console.log(sum);
 //Виконай додавання рядка "1" і цифри 2 (обидві операнди повинні бути в змінних), добийся математично правильної відповіді.
 const nan3 = '1';
 const nan4 = 2;
-console.log(+nan3 + nan4);
+const sum2 = (+nan3 + nan4);
+console.log(sum2);
 
 
 
@@ -17,4 +18,5 @@ console.log(+nan3 + nan4);
 //Користувач вказує обсяг флешки в Гб. Програма повинна порахувати скільки файлів розміром в 820 Мб поміщається на флешку.
 const memorycard = prompt("what is the size of the memory card?", "");
 const nan5 = 820;
-console.log((memorycard / nan5).toFixed(2));
+const sum3 = ((memorycard / nan5).toFixed(2));
+console.log(sum3);
