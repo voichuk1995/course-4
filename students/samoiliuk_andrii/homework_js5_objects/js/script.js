ccaa49850fae10307036000c1ae6ee972d4f644c
Andrii Samoiliuk <andrewsamoiliuk@gmail.com>
2022-09-22 00:24:59 +0300

    maximum completed

--- students/samoiliuk_andrii/homework_js5_objects/js/script.js
+++ students/samoiliuk_andrii/homework_js5_objects/js/script.js
@@ -1,66 +1,66 @@
-// // Мінімум
+// Мінімум
 
-// // Створи об'єкт, що описує автомобіль (виробник, модель, рік випуску, середня швидкість, обсяг паливного баку, середня витрата палива на 100 км., водії), і наступні методи для роботи з цим об'єктом:
-// // Метод, який виводить на екран інформацію про автомобіль.
-// // Додавання ім’я водія у список
-// // Перевірка водія на наявність його ім’я у списку
-// // Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
+// Створи об'єкт, що описує автомобіль (виробник, модель, рік випуску, середня швидкість, обсяг паливного баку, середня витрата палива на 100 км., водії), і наступні методи для роботи з цим об'єктом:
+// Метод, який виводить на екран інформацію про автомобіль.
+// Додавання ім’я водія у список
+// Перевірка водія на наявність його ім’я у списку
+// Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
 
-// const car = {
-//     vendor: "Toyota",
-//     model: "Camry",
-//     year: "2008",
-//     avgSpeed: 100,
-//     fuelTank: 60,
-//     consumption: 11,
-//     drivers: "Andrii, Maryna, Nastia",
+const car = {
+    vendor: "Toyota",
+    model: "Camry",
+    year: "2008",
+    avgSpeed: 100,
+    fuelTank: 60,
+    consumption: 11,
+    drivers: "Andrii, Maryna, Nastia",
 
-//     displayInfo: function () {
-//         let answer = "Car information:\n"
-//         for (let key in this) {
-//             if (typeof this[key] !== 'function') {
-//                 answer = answer + key + ": " + this[key] + "\n";
-//             }
-//         }
-//         console.log(answer);
-//     },
+    displayInfo: function () {
+        let answer = "Car information:\n"
+        for (let key in this) {
+            if (typeof this[key] !== 'function') {
+                answer = answer + key + ": " + this[key] + "\n";
+            }
+        }
+        console.log(answer);
+    },
 
-//     addUser: function (name) {
-//         this.drivers = this.drivers + ", " + name;
-//     },
+    addUser: function (name) {
+        this.drivers = this.drivers + ", " + name;
+    },
 
-//     checkUser: function (name) {
-//         if (this.drivers.search(name) === -1) {
-//             return "Водій не знайдений";
-//         } else {
-//             return "Водій знайдений";
-//         };
-//     },
+    checkUser: function (name) {
+        if (this.drivers.search(name) === -1) {
+            return "Водій не знайдений";
+        } else {
+            return "Водій знайдений";
+        };
+    },
 
-//     timeAndFuelCalculation: function (distance) {
-//         const time = distance / this.avgSpeed;
-//         let restTime = (time - time % 4) / 4;
-//         if (!(time % 4)) { restTime = restTime - 1 };
-//         let totalTime = time + restTime;
-//         if (totalTime < 0) { totalTime = 0 };
-//         const hours = totalTime - totalTime % 1;
-//         const minutes = (totalTime % 1) * 60;
-//         let answer = "Час поїздки з урахуванням відпочинку: " + hours.toFixed(0) + " годин " + minutes.toFixed(0) + " хвилин\n";
-//         const fuel = this.consumption / 100 * distance;
-//         answer = answer + "Витрата пального у літрах: " + fuel.toFixed(1);
-//         return answer;
-//     }
+    timeAndFuelCalculation: function (distance) {
+        const time = distance / this.avgSpeed;
+        let restTime = (time - time % 4) / 4;
+        if (!(time % 4)) { restTime = restTime - 1 };
+        let totalTime = time + restTime;
+        if (totalTime < 0) { totalTime = 0 };
+        const hours = totalTime - totalTime % 1;
+        const minutes = (totalTime % 1) * 60;
+        let answer = "Час поїздки з урахуванням відпочинку: " + hours.toFixed(0) + " годин " + minutes.toFixed(0) + " хвилин\n";
+        const fuel = this.consumption / 100 * distance;
+        answer = answer + "Витрата пального у літрах: " + fuel.toFixed(1);
+        return answer;
+    }
 
-// }
+}
 
-// car.displayInfo();
+car.displayInfo();
 
-// console.log(car.drivers);
-// car.addUser(prompt("Введіть імʼя нового водія", "Masha"));
-// console.log(car.drivers);
+console.log(car.drivers);
+car.addUser(prompt("Введіть імʼя нового водія", "Masha"));
+console.log(car.drivers);
 
-// console.log(car.checkUser(prompt("Введіть імʼя водія для перевірки", "Masha")));
-// console.log(car.timeAndFuelCalculation(+prompt("Введіть відстань в кілометрах", "")));
+console.log(car.checkUser(prompt("Введіть імʼя водія для перевірки", "Masha")));
+console.log(car.timeAndFuelCalculation(+prompt("Введіть відстань в кілометрах", "")));
 
 
 
@@ -143,10 +143,6 @@ time.addHours(prompt("Скільки годин додати", "25"));
 time.display();
 
 
-
-
-
-
 // Максимум
 
 // Створи об'єкт, що описує звичайний дріб. Створи об'єкт, який має методи роботи з дробом:
@@ -156,3 +152,98 @@ time.display();
 // Ділення 2-х об'єктів-дробів.
 // Скорочення об'єкта-дробу.
 // (Тобі потрібно буде створити ще деякі методи не зазначені в завданні, для отримання математично правильної відповіді)
+
+
+const drib = {
+    create: function (nom, denom) {
+        if (isInteger(nom) && isInteger(denom)) {
+            const a = {
+                numerator: nom,
+                denominator: denom,
+            }
+            this.simplify(a);
+            return a;
+        }
+    },
+
+    display: function (drib) {
+        if (drib.numerator === 0) {
+            console.log("0")
+        } else if (Math.abs(drib.numerator) === Math.abs(drib.denominator)) {
+            console.log(drib.numerator / drib.denominator)
+        } else {
+            console.log(drib.numerator + '/' + drib.denominator);
+        }
+    },
+
+    simplify: function (obj) {
+        let digitFive = Math.abs(obj.numerator);
+        let digitSix = Math.abs(obj.denominator);
+        if ((digitFive !== digitSix) && digitSix != 0 && digitFive != 0) {
+            do {
+                if (digitFive > digitSix) {
+                    digitFive = digitFive - digitSix;
+                } else {
+                    digitSix = digitSix - digitFive;
+                }
+            }
+            while (digitFive !== digitSix);
+        }
+        obj.numerator = obj.numerator / digitSix;
+        obj.denominator = obj.denominator / digitSix;
+        return obj;
+    },
+
+    add: function (dribFirst, dribSecond) {
+        const a = {
+            numerator: dribFirst.numerator * dribSecond.denominator + dribSecond.numerator * dribFirst.denominator,
+            denominator: dribFirst.denominator * dribSecond.denominator,
+        }
+        this.simplify(a);
+        return a;
+    },
+
+    subtract: function (dribFirst, dribSecond) {
+        const a = {
+            numerator: dribFirst.numerator * dribSecond.denominator - dribSecond.numerator * dribFirst.denominator,
+            denominator: dribFirst.denominator * dribSecond.denominator,
+        }
+        this.simplify(a);
+        return a;
+    },
+    multiply: function (dribFirst, dribSecond) {
+        const a = {
+            numerator: dribFirst.numerator * dribSecond.numerator,
+            denominator: dribFirst.denominator * dribSecond.denominator,
+        }
+        this.simplify(a);
+        return a;
+    },
+    divide: function (dribFirst, dribSecond) {
+        const a = {
+            numerator: dribFirst.numerator * dribSecond.denominator,
+            denominator: dribFirst.denominator * dribSecond.numerator,
+        }
+        this.simplify(a);
+        return a;
+    }
+}
+
+const f = drib.create(+prompt("Введіть чисельник першого дробу", "3"), +prompt("Введіть знаменник першого дробу", "5"));
+const g = drib.create(+prompt("Введіть чисельник другого дробу", "4"), +prompt("Введіть другого першого дробу", "10"));
+console.log("Введені дроби:");
+drib.display(f);
+drib.display(g);
+
+const h = drib.add(f, g);
+console.log("Сума:");
+drib.display(h);
+const k = drib.subtract(f, g);
+console.log("Різниця:");
+drib.display(k);
+const l = drib.multiply(f, g);
+console.log("Добуток:");
+drib.display(l);
+const m = drib.divide(f, g);
+console.log("Частка:");
+drib.display(m);
