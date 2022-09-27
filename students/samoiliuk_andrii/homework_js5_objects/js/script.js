ce91add1693301deb0006839d4c5652c27043c5c
Andrii Samoiliuk <andrewsamoiliuk@gmail.com>
2022-09-27 23:47:23 +0300

    remarks corrected

--- students/samoiliuk_andrii/homework_js5_objects/js/script.js
+++ students/samoiliuk_andrii/homework_js5_objects/js/script.js
@@ -1,10 +1,10 @@
-// Мінімум
+// // Мінімум
 
-// Створи об'єкт, що описує автомобіль (виробник, модель, рік випуску, середня швидкість, обсяг паливного баку, середня витрата палива на 100 км., водії), і наступні методи для роботи з цим об'єктом:
-// Метод, який виводить на екран інформацію про автомобіль.
-// Додавання ім’я водія у список
-// Перевірка водія на наявність його ім’я у списку
-// Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
+// // Створи об'єкт, що описує автомобіль (виробник, модель, рік випуску, середня швидкість, обсяг паливного баку, середня витрата палива на 100 км., водії), і наступні методи для роботи з цим об'єктом:
+// // Метод, який виводить на екран інформацію про автомобіль.
+// // Додавання ім’я водія у список
+// // Перевірка водія на наявність його ім’я у списку
+// // Підрахунок необхідного часу та кількості палива для подолання переданої відстані з середньою швидкістю. Враховуй, що через кожні 4 години дороги водієві необхідно робити перерву на 1 годину. 
 
 const car = {
     vendor: "Toyota",
@@ -31,9 +31,9 @@ const car = {
 
     checkUser: function (name) {
         if (this.drivers.search(name) === -1) {
-            return "Водій не знайдений";
+            return false;
         } else {
-            return "Водій знайдений";
+            return true;
         };
     },
 
@@ -75,7 +75,6 @@ console.log(car.timeAndFuelCalculation(+prompt("Введіть відстань 
 // Враховуйте, що в останніх 3-х функціях, при зміні однієї частини часу, може змінитися і інша. Наприклад: якщо до часу «20:59:45» додати 30 секунд, то повинно вийти «21:00:15», а не «20:59:75». Також потрібно передбачити можливість того що користувач може передати 150 секунд, або 75 хвилин.
 
 function isInteger(a) {
-    a = +a;
     if (!isNaN(a) && (!(a % 1)) && (a >= 0)) {
         return true;
     } else { return false }
@@ -86,89 +85,69 @@ const time = {
     hours: 0,
     minutes: 0,
     seconds: 0,
+
     display: function () {
         console.log(this.hours + ':' + this.minutes + ':' + this.seconds);
     },
-    validate: function () {
-        let seconds = +this.seconds;
-        let minutes = +this.minutes;
-        let hours = +this.hours;
-        if (seconds >= 60) {
-            minutes = minutes + (seconds - seconds % 60) / 60;
-            seconds = seconds % 60;
-        }
-        if (minutes >= 60) {
-            hours = hours + (minutes - minutes % 60) / 60;
-            minutes = minutes % 60;
-        }
-        if (hours >= 24) {
-            hours = hours % 24;
-        }
 
-        this.seconds = seconds;
-        this.minutes = minutes;
-        this.hours = hours;
-    },
     addSeconds: function (a) {
         if (isInteger(a)) {
-            let seconds = +this.seconds;
-            seconds = seconds + +a;
-            this.seconds = seconds;
-            this.validate();
+            this.seconds = this.seconds + a;
         }
-    },
-    addMinutes: function (a) {
-        if (isInteger(a)) {
-            let minutes = +this.minutes;
-            minutes = minutes + +a;
-            this.minutes = minutes;
-            this.validate();
-        }
-    },
-    addHours: function (a) {
-        if (isInteger(a)) {
-            this.hours = +this.hours + +a;
-            this.validate();
+        if (this.seconds >= 60) {
+            let minutes = (this.seconds - this.seconds % 60) / 60;
+            this.seconds = this.seconds - minutes * 60;
+            this.addMinutes(minutes);
         }
     },
 
+    addMinutes: function (a) {
+        if (isInteger(a)) {
+            this.minutes = this.minutes + a;
+        }
+        if (this.minutes >= 60) {
+            let hours = (this.minutes - this.minutes % 60) / 60;
+            this.minutes = this.minutes - hours * 60;
+            this.addHours(hours);
+        }
+    },
+
+    addHours: function (a) {
+        if (isInteger(a)) {
+            this.hours = this.hours + a;
+            if (this.hours >= 24) {
+                this.hours = this.hours % 24;
+            }
+        }
+    },
 
 }
 time.display();
-time.addSeconds(prompt("Скільки секунд додати", "45"));
+time.addSeconds(+prompt("Скільки секунд додати", "45"));
 time.display();
-time.addMinutes(prompt("Скільки хвилин додати", "45"));
+time.addMinutes(+prompt("Скільки хвилин додати", "45"));
 time.display();
-time.addHours(prompt("Скільки годин додати", "25"));
+time.addHours(+prompt("Скільки годин додати", "25"));
 time.display();
 
 
-// Максимум
+// // Максимум
 
-// Створи об'єкт, що описує звичайний дріб. Створи об'єкт, який має методи роботи з дробом:
-// Складання 2-х об'єктів-дробів.
-// Віднімання 2-х об'єктів-дробів.
-// Множення 2-х об'єктів-дробів.
-// Ділення 2-х об'єктів-дробів.
-// Скорочення об'єкта-дробу.
-// (Тобі потрібно буде створити ще деякі методи не зазначені в завданні, для отримання математично правильної відповіді)
+// // Створи об'єкт, що описує звичайний дріб. Створи об'єкт, який має методи роботи з дробом:
+// // Складання 2-х об'єктів-дробів.
+// // Віднімання 2-х об'єктів-дробів.
+// // Множення 2-х об'єктів-дробів.
+// // Ділення 2-х об'єктів-дробів.
+// // Скорочення об'єкта-дробу.
+// // (Тобі потрібно буде створити ще деякі методи не зазначені в завданні, для отримання математично правильної відповіді)
 
 
 const drib = {
-    create: function (nom, denom) {
-        if (isInteger(nom) && isInteger(denom)) {
-            const a = {
-                numerator: nom,
-                denominator: denom,
-            }
-            this.simplify(a);
-            return a;
-        }
-    },
-
     display: function (drib) {
         if (drib.numerator === 0) {
             console.log("0")
+        } else if (drib.denominator === 1) {
+            console.log(drib.numerator)
         } else if (Math.abs(drib.numerator) === Math.abs(drib.denominator)) {
             console.log(drib.numerator / drib.denominator)
         } else {
@@ -199,7 +178,6 @@ const drib = {
             numerator: dribFirst.numerator * dribSecond.denominator + dribSecond.numerator * dribFirst.denominator,
             denominator: dribFirst.denominator * dribSecond.denominator,
         }
-        this.simplify(a);
         return a;
     },
 
@@ -208,7 +186,6 @@ const drib = {
             numerator: dribFirst.numerator * dribSecond.denominator - dribSecond.numerator * dribFirst.denominator,
             denominator: dribFirst.denominator * dribSecond.denominator,
         }
-        this.simplify(a);
         return a;
     },
     multiply: function (dribFirst, dribSecond) {
@@ -216,7 +193,6 @@ const drib = {
             numerator: dribFirst.numerator * dribSecond.numerator,
             denominator: dribFirst.denominator * dribSecond.denominator,
         }
-        this.simplify(a);
         return a;
     },
     divide: function (dribFirst, dribSecond) {
@@ -224,13 +200,25 @@ const drib = {
             numerator: dribFirst.numerator * dribSecond.denominator,
             denominator: dribFirst.denominator * dribSecond.numerator,
         }
-        this.simplify(a);
         return a;
     }
 }
 
-const f = drib.create(+prompt("Введіть чисельник першого дробу", "3"), +prompt("Введіть знаменник першого дробу", "5"));
-const g = drib.create(+prompt("Введіть чисельник другого дробу", "4"), +prompt("Введіть другого першого дробу", "10"));
+const nomFirst = +prompt("Введіть чисельник першого дробу", "2");
+const denomFirst = +prompt("Введіть знаменник першого дробу", "3");
+const nomSecond = +prompt("Введіть чисельник другого дробу", "5");
+const denomSecond = +prompt("Введіть знаменник другого дробу", "8");
+
+const f = {
+    numerator: nomFirst,
+    denominator: denomFirst,
+}
+
+const g = {
+    numerator: nomSecond,
+    denominator: denomSecond,
+}
+
 console.log("Введені дроби:");
 drib.display(f);
 drib.display(g);
@@ -238,12 +226,28 @@ drib.display(g);
 const h = drib.add(f, g);
 console.log("Сума:");
 drib.display(h);
+console.log("Спрощена сума:");
+drib.display(drib.simplify(h));
+
+
 const k = drib.subtract(f, g);
 console.log("Різниця:");
 drib.display(k);
+console.log("Спрощена різниця:");
+drib.display(drib.simplify(k));
+
+
 const l = drib.multiply(f, g);
 console.log("Добуток:");
 drib.display(l);
+console.log("Спрощений добуток")
+drib.display(drib.simplify(l));
+
+
+
+
 const m = drib.divide(f, g);
 console.log("Частка:");
 drib.display(m);
+console.log("Спрощена частка:");
+drib.display(drib.simplify(m));
