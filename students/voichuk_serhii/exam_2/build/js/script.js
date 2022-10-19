5d09b0418e4ee64de888313b3c6c2dbaa04379e2
voichuk1995 <S1e9r9u5ypro100easy>
2022-10-19 17:35:06 +0300

    add contact icon

--- students/voichuk_serhii/exam_2/build/js/script.js
+++ students/voichuk_serhii/exam_2/build/js/script.js
@@ -284,23 +284,37 @@ function headerSwiper() {
 __webpack_require__.r(__webpack_exports__);
 function myScroll() {
   window.addEventListener('scroll', function () {
-    var scrolltop = scrollY;
+    var scrollTop = scrollY;
     var header = document.querySelector('.header');
-    var arrowTop = document.querySelector('.footer__go-top');
-    if (scrolltop >= 370) {
+    if (scrollTop >= 370) {
       header.classList.add('hide');
     } else {
       header.classList.remove('hide');
     }
     ;
-    if (scrolltop >= 22) {
+    var arrowTop = document.querySelector('.footer__go-top');
+    if (scrollTop >= 22) {
       arrowTop.classList.add('hide-arrow');
     } else {
       arrowTop.classList.remove('hide-arrow');
     }
     ;
+
+    // document.querySelectorAll('.forScroll').forEach((el, i) => {
+    //     if(el.offsetTop - document.querySelector('.menu__list').clientHeight <= scrollTop){
+    //         document.querySelectorAll('.menu__link').forEach((el) => {
+    //             if(el.classList.contains('menu__link--active')){
+    //                 el.classList.remove('menu__link--active')
+    //             }
+    //         })
+    //     }
+
+    //     document.querySelectorAll('.menu__link').classList.add('menu__link--active');
+    // });
   });
 }
+
+;
 /* harmony default export */ __webpack_exports__["default"] = (myScroll);
 
 /***/ }),
--- students/voichuk_serhii/exam_2/src/js/_myScroll.js
+++ students/voichuk_serhii/exam_2/src/js/_myScroll.js
@@ -1,24 +1,41 @@
 function myScroll(){
 
     window.addEventListener('scroll', function() {
-        const scrolltop = scrollY;
+        const scrollTop = scrollY;
         
         const header = document.querySelector('.header');
-        const arrowTop = document.querySelector('.footer__go-top');
-
-        if(scrolltop >= 370){
+        
+        if(scrollTop >= 370){
             header.classList.add('hide');
         }else{    
             header.classList.remove('hide');
         };
 
-        if(scrolltop >= 22){
+
+
+        const arrowTop = document.querySelector('.footer__go-top');
+
+        if(scrollTop >= 22){
             arrowTop.classList.add('hide-arrow');
         }else{    
             arrowTop.classList.remove('hide-arrow');
         };
+
+
+        // document.querySelectorAll('.forScroll').forEach((el, i) => {
+        //     if(el.offsetTop - document.querySelector('.menu__list').clientHeight <= scrollTop){
+        //         document.querySelectorAll('.menu__link').forEach((el) => {
+        //             if(el.classList.contains('menu__link--active')){
+        //                 el.classList.remove('menu__link--active')
+        //             }
+        //         })
+        //     }
+
+        //     document.querySelectorAll('.menu__link').classList.add('menu__link--active');
+        // });
+
     });
 
-}
+};
 
 export default myScroll;
