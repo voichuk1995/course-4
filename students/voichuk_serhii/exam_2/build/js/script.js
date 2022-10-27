dd298279e100e1ae26596240d9562850c8d78b0d
voichuk1995 <S1e9r9u5ypro100easy>
2022-10-27 21:47:21 +0300

    adaptiv p5

--- students/voichuk_serhii/exam_2/build/js/script.js
+++ students/voichuk_serhii/exam_2/build/js/script.js
@@ -408,14 +408,29 @@ function newsSlicker() {
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
-      // autoplay: true,
+      autoplay: true,
       autoplaySpeed: 4000,
       dots: true,
       responsive: [{
         breakpoint: 1200,
         settings: {
           slidesToShow: 2,
-          centerMode: true
+          centerMode: true,
+          centerPadding: "30px"
+        }
+      }, {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true,
+          centerPadding: "10px"
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          centerPadding: "5px"
         }
       }]
     });
--- students/voichuk_serhii/exam_2/src/js/_newsSlicker.js
+++ students/voichuk_serhii/exam_2/src/js/_newsSlicker.js
@@ -5,7 +5,7 @@ function newsSlicker(){
           infinite: true,
           slidesToShow: 3,
           slidesToScroll: 1,
-          // autoplay: true,
+          autoplay: true,
           autoplaySpeed: 4000,
           dots: true,
           responsive: [
@@ -14,6 +14,23 @@ function newsSlicker(){
               settings: {
                 slidesToShow: 2,
                 centerMode: true,
+                centerPadding: "30px",
+              },
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                centerMode: true,
+                centerPadding: "10px",
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                centerMode: true,
+                centerPadding: "5px",
               },
             },
           ]
